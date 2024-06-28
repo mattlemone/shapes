baa360eef25012d608e56933628ab1c618f12888
mattlemone <lemonbaseball@gmail.com>
2024-06-27 20:37:51 -0600

    Created triangle tests, testing all pieces of triangle

--- src/test/kotlin/TriangleTest.kt
+++ src/test/kotlin/TriangleTest.kt
@@ -1,2 +1,54 @@
+package org.example
+
+import Point
+import Triangle
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
 class TriangleTest {
+
+    @Test
+    fun testTriangleCreation() {
+        val pointA = Point(0.0, 0.0)
+        val pointB = Point(4.0, 0.0)
+        val pointC = Point(2.0, 3.0)
+        val triangle = Triangle(pointA, pointB, pointC)
+        assertEquals(pointA, triangle.pointA)
+        assertEquals(pointB, triangle.pointB)
+        assertEquals(pointC, triangle.pointC)
+    }
+
+    @Test
+    fun testInvalidTriangleCreation() {
+        val pointA = Point(0.0, 0.0)
+        val pointB = Point(4.0, 0.0)
+        val pointC = Point(8.0, 0.0)
+        assertThrows(IllegalArgumentException::class.java) {
+            Triangle(pointA, pointB, pointC)
+        }
+    }
+
+    @Test
+    fun testGetArea() {
+        val pointA = Point(0.0, 0.0)
+        val pointB = Point(4.0, 0.0)
+        val pointC = Point(2.0, 3.0)
+        val triangle = Triangle(pointA, pointB, pointC)
+        assertEquals(6.0, triangle.getArea())
+    }
+
+    @Test
+    fun testMove() {
+        val pointA = Point(0.0, 0.0)
+        val pointB = Point(4.0, 0.0)
+        val pointC = Point(2.0, 3.0)
+        val triangle = Triangle(pointA, pointB, pointC)
+        triangle.move(2.0, 3.0)
+        assertEquals(2.0, triangle.pointA.x)
+        assertEquals(3.0, triangle.pointA.y)
+        assertEquals(6.0, triangle.pointB.x)
+        assertEquals(3.0, triangle.pointB.y)
+        assertEquals(4.0, triangle.pointC.x)
+        assertEquals(6.0, triangle.pointC.y)
+    }
 }
